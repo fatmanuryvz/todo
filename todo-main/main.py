adcee5c906491016ac7df71f7e54ad3a69158e51
fatmanuryvz <fatmanuryvz.3535@gmail.com>
2026-03-23 09:59:54 +0300

    Tamamlandı

--- todo-main/main.py
+++ todo-main/main.py
@@ -1,9 +1,19 @@
 from database import Base, engine
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.controller import todo
 from src.api.controller import auth
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(todo.router)
